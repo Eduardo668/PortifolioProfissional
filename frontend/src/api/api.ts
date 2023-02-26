e76c4f7b3274177c8480a09b6cddac32e320e236
Eduardo668 <eduardosehn20@gmail.com>
2023-02-26 12:59:14 -0300

    RELEASE

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const Api = axios.create(
     {
-        baseURL:"https://8081-eduardo668-portifoliopr-7lrly9bx8il.ws-us88.gitpod.io"
+        baseURL:"https://portifolio-profissional-api.fly.dev"
     }
 )
