c3e25342bb173b79af11f857bee9f2697f900f76
Eduardo668 <eduardosehn20@gmail.com>
2023-03-20 14:49:53 -0300

    RELEASE

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const Api = axios.create(
     {
-        baseURL:"https://ec2-18-228-26-91.sa-east-1.compute.amazonaws.com"
+        baseURL:"https://ec2-18-228-12-130.sa-east-1.compute.amazonaws.com"
     }
 )
